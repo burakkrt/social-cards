bb3eb100d489c915422a8e813e5507a6f3dc2396
burakkrt <burak.kurt@glomil.com>
2023-11-10 00:33:49 +0300

    agenda tweet component complated

--- src/components/tweet-card/TweetCard.style.jsx
+++ src/components/tweet-card/TweetCard.style.jsx
@@ -1,5 +1,167 @@
 import styled from 'styled-components'
 
 export const AgendaTweetCard = styled.div`
-  color: red;
+  max-width: 512px;
+
+  .agenda-tweet {
+    &-header {
+      display: flex;
+      flex-direction: row;
+      justify-content: start;
+      align-items: center;
+      gap: 2.3rem;
+      position: relative;
+      background: #000000 0% 0% no-repeat padding-box;
+      border-radius: 20px 20px 0px 0px;
+      padding: 1.7rem 2.6rem;
+      z-index: -2;
+
+      &-profile-image {
+        position: relative;
+        img {
+          width: 62px;
+          border-radius: 50%;
+        }
+        &::after {
+          content: '';
+          position: absolute;
+          width: 74px;
+          height: 74px;
+          left: calc(((74px - 62px) / 2) * -1);
+          top: calc(((74px - 62px) / 2) * -1);
+          background: #ffffff 0 0 no-repeat padding-box;
+          border-radius: 50%;
+          z-index: -1;
+        }
+      }
+
+      &-profile-des {
+        &-name {
+          display: block;
+          font: normal normal bold 20px/25px Euclid Circular A;
+          color: #ffffff;
+        }
+        &-link {
+          display: block;
+          font: normal normal 18px/18px Euclid Circular A;
+          color: #707b93;
+        }
+      }
+    }
+
+    &-body {
+      padding: 1.7rem 2.6rem;
+      background: #ffffff 0 0 no-repeat padding-box;
+      border: 1px solid #e8e8e8;
+      font: normal normal normal 18px/23px Euclid Circular A;
+
+      &-post-image {
+        width: 100%;
+        border-radius: 20px;
+        margin: 1rem 0;
+      }
+
+      &-post-text {
+        margin: 0;
+      }
+
+      &-post-infos {
+        color: #707b93;
+        margin-top: 1rem;
+        span:not(:first-child) {
+          margin-left: 8px;
+          &::before {
+            content: '|';
+            color: #707b93;
+            margin-right: 8px;
+          }
+          strong {
+            color: #000000;
+          }
+        }
+      }
+    }
+
+    &-footer {
+      position: relative;
+      &-statistics {
+        position: relative;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        gap: 3rem;
+        background: #f4f4f4 0 0 no-repeat padding-box;
+        padding: 1rem 0;
+        z-index: -1111;
+
+        span {
+          font: normal normal 18px/23px Euclid Circular A;
+
+          strong {
+            margin-right: 0.4rem;
+          }
+        }
+      }
+      &-link {
+        background: #000000 0 0 no-repeat padding-box;
+        border-radius: 0 0 20px 20px;
+        width: 100%;
+        padding: 1rem 0;
+
+        button {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          gap: 4px;
+          font: normal normal 16px/20px Euclid Circular A;
+          margin: auto;
+          color: #959698;
+
+          &:hover {
+            color: #ffffff;
+          }
+        }
+
+        img {
+          width: 18px;
+          color: #959698;
+        }
+      }
+      &-logo {
+        position: absolute;
+        left: 28px;
+        bottom: 9px;
+        img {
+          position: relative;
+          width: 38px;
+          height: 38px;
+          border-radius: 50%;
+          border: 1px solid #ffffff;
+          padding: 4px;
+        }
+        &::after {
+          content: '';
+          position: absolute;
+          width: 64px;
+          height: 64px;
+          left: calc(((64px - 48px) / 2) * -1);
+          top: calc(((64px - 48px) / 2) * -1);
+          background: #000000 0 0 no-repeat padding-box;
+          border-radius: 50%;
+          z-index: -111;
+        }
+      }
+    }
+  }
+  .btn {
+    background: none;
+    border: none;
+    cursor: pointer;
+    transition-duration: 200ms;
+    padding: 0;
+  }
+  .btn.btn-primary {
+    color: #1ea4ea !important;
+  }
 `
--- src/components/tweet-card/TweetCard.tsx
+++ src/components/tweet-card/TweetCard.tsx
@@ -1,10 +1,109 @@
 'use client'
 import * as Styled from './TweetCard.style'
+import ozdagProfil from './images/ozdag-profil.jpg'
+import xLogo from './images/x-logo.svg'
+import openLink from './images/open-link.svg'
+import postImage from './images/post-image.svg'
+import { useEffect } from 'react'
 export default function TweetCard() {
+  // useEffect(() => {
+  //   const desc: HTMLElement | null = document.querySelector(
+  //     '.agenda-tweet-body-post-text',
+  //   )
+  //   if (desc) {
+  //     const value: string | null = desc.textContent
+  //     let profileText = ''
+  //     if (value && value.indexOf('@') > 0) {
+  //       for (let i = value.indexOf('@'); i < value.length; i++) {
+  //         if (value.charAt(i) === ' ') {
+  //           console.log('okey')
+  //           profileText = value.slice(value.indexOf('@'), i)
+  //           break
+  //         }
+  //       }
+  //     }
+  //     if (value && value.indexOf('@') > 0 && profileText.length === 0) {
+  //       profileText = value.slice(value.indexOf('@'))
+  //     }
+  //     if (value && profileText.length > 0) {
+  //       desc.innerHTML = value.replace(
+  //         profileText,
+  //         `<a href="https://twitter.com/${profileText}" target="_blank">${profileText}</a>`,
+  //       )
+  //     }
+  //   }
+  // }, [])
+
   return (
     <Styled.AgendaTweetCard>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
-      ipsum modi nisi rem vero.
+      <div className="agenda-tweet-header">
+        <div className="agenda-tweet-header-profile-image">
+          <img src={ozdagProfil.src} alt="Umit Ozdag" />
+        </div>
+        <div className="agenda-tweet-header-profile-des">
+          <span className="agenda-tweet-header-profile-des-name">
+            Ümit Özdağ
+          </span>
+          <span className="agenda-tweet-header-profile-des-link">
+            @umitozdag
+          </span>
+        </div>
+      </div>
+      <div className="agenda-tweet-body">
+        <p className="agenda-tweet-body-post-text">
+          İsrail’in Gazze’de gerçekleştirdiği soykırım ve etnik temizlik
+          karşısında miting yapmak ve çok kızmaktan başka hiç bir şey yapmayan
+          AK Parti şimdi İsrail’in Gazze’yi boşaltma planına yardımcı olacak
+          şekilde hasta tedavisi, yetim çocuklar söylemleri ile kamuoyunu
+          Gazzelilerin bir kısmının Türkiye’ye gelmesine hazırlıyor. Gazzelileri
+          Türkiye’ye ne ad altında olursa olsun getirmek Gazze’yi boşaltmak
+          isteyen İsrail’e hizmettir. @umitozdag
+        </p>
+        <img
+          className="agenda-tweet-body-post-image"
+          src={postImage.src}
+          alt="Post image"
+        />
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={() => alert('button passive')}
+        >
+          Çeviri yap
+        </button>
+        <div className="agenda-tweet-body-post-infos">
+          <span>ÖS 10:54 - 6 Kas 2023</span>
+          <span>
+            <strong>1.6 Mn</strong> Görüntülenme
+          </span>
+        </div>
+      </div>
+      <div className="agenda-tweet-footer">
+        <div className="agenda-tweet-footer-statistics">
+          <span>
+            <strong>58</strong> Repost
+          </span>
+          <span>
+            <strong>9</strong> Alıntı
+          </span>
+          <span>
+            <strong>254</strong> Beğeni
+          </span>
+        </div>
+        <div className="agenda-tweet-footer-link">
+          <button
+            className="btn"
+            type="button"
+            onClick={() => alert('button passive')}
+          >
+            Post 'u görüntülemek için tıklayınız
+            <img src={openLink.src} alt="Open link logo" />
+          </button>
+        </div>
+        <div className="agenda-tweet-footer-logo">
+          <img src={xLogo.src} alt="X Logo" />
+        </div>
+      </div>
     </Styled.AgendaTweetCard>
   )
 }
