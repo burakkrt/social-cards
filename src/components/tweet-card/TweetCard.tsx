16d2379254f5177ccdf2f36436689a699a017fc1
burakkrt <burak.kurt@glomil.com>
2023-11-09 21:48:47 +0300

    skeleton

---
+++ src/components/tweet-card/TweetCard.tsx
@@ -0,0 +1,10 @@
+'use client'
+import * as Styled from './TweetCard.style'
+export default function TweetCard() {
+  return (
+    <Styled.AgendaTweetCard>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
+      ipsum modi nisi rem vero.
+    </Styled.AgendaTweetCard>
+  )
+}
