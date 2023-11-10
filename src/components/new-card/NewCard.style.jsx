44f2a5f51064e081fefb0b56692309262ba19235
burakkrt <burak.kurt@glomil.com>
2023-11-10 11:42:23 +0300

    creat newCard completed

---
+++ src/components/new-card/NewCard.style.jsx
@@ -0,0 +1,92 @@
+import styled from 'styled-components'
+
+export const AgendaNewCard = styled.div`
+  max-width: 512px;
+  border: 1px solid #e8e8e8;
+  border-radius: 20px;
+
+  .agenda-new {
+    &-header {
+      display: flex;
+      flex-direction: row;
+      justify-content: start;
+      align-items: center;
+      gap: 1rem;
+      border-radius: 20px 20px 0 0;
+      padding: 1.5rem 2.5rem 0.5rem 2.5rem;
+
+      &-profile-image {
+        position: relative;
+        padding: 10px 10px;
+        img {
+          width: 44px;
+          border-radius: 50%;
+          background-color: #ffffff;
+        }
+        &::after {
+          content: '';
+          position: absolute;
+          width: 58px;
+          height: 58px;
+          left: calc(((58px - 44px - 20px) / 2) * -1);
+          top: calc(((58px - 44px - 20px) / 2) * -1);
+          background: #000000 0 0 no-repeat padding-box;
+          border-radius: 50%;
+          z-index: -1;
+        }
+      }
+
+      &-profile-new-name {
+        font: normal normal bold 26px/28px Euclid Circular A;
+      }
+
+      &-profile-link {
+        margin-left: auto;
+
+        a {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          gap: 6px;
+          font: normal normal medium 16px/20px Euclid Circular A;
+          color: #959698;
+          transition-duration: 200ms;
+          &:hover {
+            color: #000000;
+          }
+        }
+        img {
+          width: 18px;
+        }
+      }
+    }
+
+    &-body {
+      padding: 0 2.8rem 1.5rem 2.8rem;
+      &-image {
+        width: 100%;
+        border-radius: 10px;
+        margin-bottom: 0.5rem;
+      }
+
+      &-post-info {
+        font: normal normal normal 14px/18px Euclid Circular A;
+        color: #707b93;
+        margin: 0 0.2rem;
+      }
+      &-post-detail {
+        margin: 0 0.2rem;
+        &-title {
+          font: normal normal 600 22px/25px Euclid Circular A;
+          color: #2e2f31;
+          margin: 1rem 0;
+        }
+        &-text {
+          font: normal normal normal 16px/20px Euclid Circular A;
+          color: #2e2f31;
+          margin-top: 1rem;
+        }
+      }
+    }
+  }
+`
--- src/components/new-card/NewCard.tsx
+++ src/components/new-card/NewCard.tsx
@@ -1,3 +1,44 @@
+'use client'
+import * as Styled from './NewCard.style'
+import newLogo from './images/new-logo.png'
+import openLink from './images/open-link.svg'
+import newImage from './images/new-image.svg'
+
 export default function NewCard() {
-  return <div>Lorem ipsum dolor sit.</div>
+  return (
+    <Styled.AgendaNewCard>
+      <div className="agenda-new-header">
+        <div className="agenda-new-header-profile-image">
+          <img src={newLogo.src} alt="New Logo" />
+        </div>
+        <span className="agenda-new-header-profile-new-name">Milliyet</span>
+        <div className="agenda-new-header-profile-link">
+          <a href="http://localhost:8080" target="_blank">
+            Haber Linki <img src={openLink.src} alt="Open link logo" />{' '}
+          </a>
+        </div>
+      </div>
+      <div className="agenda-new-body">
+        <img
+          className="agenda-new-body-image"
+          src={newImage.src}
+          alt="New image"
+        />
+        <span className="agenda-new-body-post-info">6 Kas 2023 Pazartesi</span>
+        <div className="agenda-new-body-post-detail">
+          <h3 className="agenda-new-body-post-detail-title">
+            Bakan Ersoy: Bu afeti el ele vererek birlikte atlatacağız
+          </h3>
+          <p className="agenda-new-body-post-detail-text">
+            Kültür ve Turizm Bakanı Mehmet Nuri Ersoy, Ramazan ayının ilk
+            iftarını Malatya’nın Battalgazi ilçesi İnönü Üniversitesi Teknopark
+            bahçesinde oluşturulan konteyner kentte depremzedelerle birlikte
+            yaptı. İftar sonrası açıklamalarda bulunan Ersoy, “Ülkemiz ve
+            Malatya’mız tarihin en büyük afetlerinden biri ile yüzleşti, çok
+            büyük yıkımlar oldu.
+          </p>
+        </div>
+      </div>
+    </Styled.AgendaNewCard>
+  )
 }
