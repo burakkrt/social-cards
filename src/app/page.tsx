f933d3f779061abe036e333917894abf22dbc50a
burakkrt <burak.kurt@glomil.com>
2023-11-10 09:48:10 +0300

    creat newCard skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import TweetCard from './../components/tweet-card'
+import NewCard from './../components/new-card'
 
 export default function Home() {
   return (
-    <main style={{ margin: '48px' }}>
+    <main style={{ margin: '48px', display: 'flex', gap: '48px' }}>
       <TweetCard />
+      <NewCard />
     </main>
   )
 }
---
+++ src/components/new-card/NewCard.tsx
@@ -0,0 +1,3 @@
+export default function NewCard() {
+  return <div>Lorem ipsum dolor sit.</div>
+}
